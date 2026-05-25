80198644582184b8b7ce7d391c74f0039020c7d3
asheshp23 <asheshp23@gmail.com>
2026-05-25 16:43:43 -0400

    Initial Commit

---
+++ BikeTheCreek/BikeTheCreekApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeTheCreekApp.swift
+//  BikeTheCreek
+//
+//  Created by Ashesh Patel on 2026-05-25.
+//
+
+import SwiftUI
+
+@main
+struct BikeTheCreekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BikeTheCreekTests/BikeTheCreekTests.swift
@@ -0,0 +1,19 @@
+//
+//  BikeTheCreekTests.swift
+//  BikeTheCreekTests
+//
+//  Created by Ashesh Patel on 2026-05-25.
+//
+
+import Testing
+@testable import BikeTheCreek
+
+struct BikeTheCreekTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
